7537eefa3461f7b4823654da99c2421c8b8767e8
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2015-03-21 13:40:50 +0700

    [Documentation] Mention list of utilities in main module, add FAQ & See Also

--- lib/App/DzilUtils.pm
+++ lib/App/DzilUtils.pm
@@ -10,7 +10,21 @@ use 5.010001;
 
 =head1 SYNOPSIS
 
-This distribution provides several command-line utilities related to
-L<Dist::Zilla>.
+This distribution provides the following command-line utilities related to
+L<Dist::Zilla>:
+
+#INSERT_EXECS_LIST
+
+The main feature of these utilities is tab completion.
+
+
+=head1 FAQ
+
+#INSERT_BLOCK: App::PMUtils faq
+
+
+=head1 SEE ALSO
+
+#INSERT_BLOCK: App::PMUtils see_also
 
 =cut
